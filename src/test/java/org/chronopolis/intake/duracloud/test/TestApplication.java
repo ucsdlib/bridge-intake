b0fc7970c17c9a05eb9b5223795ede5099914c9d
Michael Ritter <shake@umiacs.umd.edu>
2017-05-30 17:10:10 -0400

    Update chronopolis dependency versions

--- src/test/java/org/chronopolis/intake/duracloud/test/TestApplication.java
+++ src/test/java/org/chronopolis/intake/duracloud/test/TestApplication.java
@@ -1,12 +1,10 @@
 package org.chronopolis.intake.duracloud.test;
 
-import org.chronopolis.common.settings.DPNSettings;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
@@ -15,7 +13,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableConfigurationProperties(IntakeSettings.class)
-@ComponentScan(basePackageClasses = {DPNSettings.class})
 public class TestApplication {
 
     public static void main(String[] args) {

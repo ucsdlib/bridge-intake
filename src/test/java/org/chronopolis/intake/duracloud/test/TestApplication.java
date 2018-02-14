014c6acd3ab3facd9d21e63bcf5db582e593afab
Michael Ritter <shake@umiacs.umd.edu>
2018-02-14 16:57:15 -0500

    Remove spring-batch annotations and dependency

--- src/test/java/org/chronopolis/intake/duracloud/test/TestApplication.java
+++ src/test/java/org/chronopolis/intake/duracloud/test/TestApplication.java
@@ -4,7 +4,6 @@ import org.chronopolis.common.storage.BagStagingProperties;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
 import org.chronopolis.intake.duracloud.config.props.BagProperties;
 import org.chronopolis.rest.api.IngestAPIProperties;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -15,7 +14,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  * Created by shake on 12/4/15.
  */
 @SpringBootApplication
-@EnableBatchProcessing
 @EnableConfigurationProperties({IntakeSettings.class, BagProperties.class, BagStagingProperties.class, IngestAPIProperties.class})
 public class TestApplication {
 

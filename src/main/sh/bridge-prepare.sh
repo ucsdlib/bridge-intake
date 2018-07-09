c4b221cb08523c5f29041e9fda0d773b530fcf2e
Michael Ritter <shake@umiacs.umd.edu>
2018-07-09 16:48:18 -0400

    Create and chown the pidfile

--- src/main/sh/bridge-prepare.sh
+++ src/main/sh/bridge-prepare.sh
@@ -2,6 +2,7 @@
 
 LOG_DIR="/var/log/chronopolis/"
 DATA_DIR="/var/lib/chronopolis/data/"
+PIDFILE="/var/run/bridge-intake.pid"
 user=`systemctl show -p User bridge-intake | sed 's/User=//'`
 group=`systemctl show -p Group bridge-intake | sed 's/Group=//'`
 
@@ -26,5 +27,9 @@ function check_dir(){
 
 check_dir ${LOG_DIR}
 check_dir ${DATA_DIR}
+
+# Just want the pid file to exist so we don't throw any exceptions
+touch ${PIDFILE}
+chown ${user}:${group} ${PIDFILE}
 
 exit 0
